a580a48335f2caa180ee144518d00aa311319012
Samarth <167466628+samjam2005@users.noreply.github.com>
2024-10-26 17:16:14 +0800

    Update BookAppointment.js

--- src/BookAppointment.js
+++ src/BookAppointment.js
@@ -1,4 +1,19 @@
+// Import Firebase functions
 import { getDatabase, ref, push, set } from "firebase/database";
+import { initializeApp } from "firebase/app";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA8vjKq3PTgY3fMelbwIVhaKqKERbCUNdc",
+  authDomain: "sc2002-5105d.firebaseapp.com",
+  projectId: "sc2002-5105d",
+  storageBucket: "sc2002-5105d.appspot.com",
+  messagingSenderId: "714442349120",
+  appId: "1:714442349120:web:fedf34767969bb69fcdda5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
 function bookAppointment(userId, hospitalName, date) {
@@ -9,7 +24,6 @@ function bookAppointment(userId, hospitalName, date) {
     date: date
   });
 }
-
 
 function addFavoriteHospital(userId, hospitalName) {
   const favHospitalsRef = ref(database, 'users/' + userId + '/favoriteHospitals/' + hospitalName);
